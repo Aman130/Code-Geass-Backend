6b135e763164213ee637789d0672a19284c719b4
Aman Mamgain <amanmamgain130@gmail.com>
2023-05-28 13:38:00 +0530

    check

--- controllers/userRouteController/checkProblemController.js
+++ controllers/userRouteController/checkProblemController.js
@@ -40,9 +40,9 @@ const checkProblemController = async (req, res) => {
         console.log(JSON.stringify(userOutput));
         console.log("---");
         console.log(JSON.stringify(problem.output));
-        problem.output=problem.output.replace(/\n/g, "\r\n");
-        console.log("---");
-        console.log(JSON.stringify(problem.output));
+        // problem.output=problem.output.replace(/\n/g, "\r\n");
+        // console.log("---");
+        // console.log(JSON.stringify(problem.output));
         if (userOutput === problem.output) {
             let addScore;
             if (problem.difficulty == "Easy") {
